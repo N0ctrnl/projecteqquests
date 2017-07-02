3ee6e6d860f45b446f32a97f886944861b0019ac
N0ctrnl <kurt@gilpin.info>
2017-07-02 04:14:05 -0500

    Updated Collector for 2-year token

--- bazaar/Collector_Dannel.pl
+++ bazaar/Collector_Dannel.pl
@@ -10,18 +10,18 @@ my $random_result = int(rand(5000));
 #    plugin::Debug("$random_result");
 
 # Buff Crystal - 0-3000
-  if($itemcount{132520} && $random_result>=0 && $random_result<4501) {
-    quest::summonitem(132521,10);
-    plugin::Whisper("This token is in fair condition. Use these crystals to fortify yourself and your companions.");
+#  if(($itemcount{132522} || $itemcount{132530} || $itemcount{132530}) && $random_result>=0 && $random_result<4501) {
+#    quest::summonitem(132521,10);
+#    plugin::Whisper("This token is in fair condition. Use these crystals to fortify yourself and your companions.");
 #    quest::gmsay("$random_result");
-  }
+#  }
 
 #  if($itemcount{132520} && $random_result>1000 && $random_result<3000) {
 #    quest::summonitem(quest::ChooseRandom(132521));
 #  }
 
 # Illusions - 3001-4700
-  elsif($itemcount{132520} && $random_result>4500 && $random_result<4901){  
+  if(($itemcount{132522} || $itemcount{132520} || $itemcount{132530}) && $random_result>4500 && $random_result<4951){  
     quest::summonitem(quest::ChooseRandom(40612,40613,40684,40685,40714,40746,40778,40779,43971,43972,54833,54913,40638));
     plugin::Whisper("This token is in fine shape. Use this item to mask your appearance.");
 #    quest::gmsay("$random_result");
@@ -33,14 +33,14 @@ my $random_result = int(rand(5000));
 # Guise of the Deceiver
 # Runed Fighter's Staff
 # Holgresh Elder Beads
-  elsif($itemcount{132520} && $random_result>4950 && $random_result<5001){  
+  elsif(($itemcount{132522} || $itemcount{132520} || $itemcount{132530}) && $random_result>4950 && $random_result<5001){  
     quest::summonitem(quest::ChooseRandom(14727,14730,2469,6630,24890));
     plugin::Whisper("This token is in fantastic condition! Here's something from my private stock!");
 #    quest::gmsay("$random_result");
   }
 
 # Buff Crystal - 0-3000
-  elsif($itemcount{132522} && $random_result>=0 && $random_result<4501) {
+  elsif(($itemcount{132522} || $itemcount{132520} || $itemcount{132530}) && $random_result>=0 && $random_result<4501) {
     quest::summonitem(132521,10);
     plugin::Whisper("This token is in fair condition. Use these crystals to fortify yourself and your companions.");
 #    quest::gmsay("$random_result");
@@ -51,22 +51,22 @@ my $random_result = int(rand(5000));
 #  }
 
 # Illusions - 3001-4700
-  elsif($itemcount{132522} && $random_result>4500 && $random_result<4951){
-    quest::summonitem(quest::ChooseRandom(40612,40613,40684,40685,40714,40746,40778,40779,43971,43972,54833,54913,40638));
-    plugin::Whisper("This token is in fine shape. Use this item to mask your appearance.");
+#  elsif($itemcount{132522} && $random_result>4500 && $random_result<4951){
+#    quest::summonitem(quest::ChooseRandom(40612,40613,40684,40685,40714,40746,40778,40779,43971,43972,54833,54913,40638));
+#    plugin::Whisper("This token is in fine shape. Use this item to mask your appearance.");
 #    quest::gmsay("$random_result");
 # 50854 - Drachnid mask - Not working right now
-  }
+#  }
 # Legacy Items - 4700-5000
 # Locket of Escape,
 # Circlet of Shadow
 # Guise of the Deceiver
 # Runed Fighter's Staff
 # Holgresh Elder Beads
-  elsif($itemcount{132522} && $random_result>4950 && $random_result<5001){
-    quest::summonitem(quest::ChooseRandom(14727,14730,2469,6630,24890));
-    plugin::Whisper("This token is in fantastic condition! Here's something from my private stock!");
+#  elsif(($itemcount{132522} || $itemcount{132530} || $itemcount{132530}) && $random_result>4950 && $random_result<5001){
+#    quest::summonitem(quest::ChooseRandom(14727,14730,2469,6630,24890));
+#    plugin::Whisper("This token is in fantastic condition! Here's something from my private stock!");
 #    quest::gmsay("$random_result");
-  }
+#  }
 
 }
--- thurgadinb/Seneschal_Aldikar.pl
+++ thurgadinb/Seneschal_Aldikar.pl
@@ -22,7 +22,8 @@ sub EVENT_SIGNAL {
 #}
 
 sub EVENT_SAY{
-  if($text=~/I will accept this task/i && $dainring == 1){
+  if($text=~/I will accept this task/i){
+# && $dainring == 1){
     quest::say("In this box, place the accursed dirk of the fallen Rodrick. With it combine the heads of every traitor you dispose of. When this is done give the box and the velium insignia ring to the Dain directly. On behalf of the crown and all good Coldain, I thank you ... May Brell be with you.");
     quest::summonitem("17055"); 
 #    quest::settimer("thurgdaynight",5);
