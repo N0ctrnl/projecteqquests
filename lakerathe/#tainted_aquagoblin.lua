4689ab98e47de18d41d69df0a27683acb5df20f2
N0ctrnl <kurt@gilpin.info>
2017-09-17 04:07:01 -0500

    Misc

--- lakerathe/#tainted_aquagoblin.lua
+++ lakerathe/#tainted_aquagoblin.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
-	eq.unique_spawn(51002,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+	eq.unique_spawn(51149,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 end
 
 -------------------------------------------------------------------------------------------------
--- sleeper/#Nanzata_the_Warder.pl
+++ sleeper/#Nanzata_the_Warder.pl
@@ -1,12 +1,11 @@
-sub EVENT_DEATH_COMPLETE {
 $ventani = $entity_list->GetMobByNpcTypeID(128091);
 $tukaarak = $entity_list->GetMobByNpcTypeID(128092);
 $hraashna = $entity_list->GetMobByNpcTypeID(128093);
+sub EVENT_DEATH_COMPLETE {
 if (!$ventani && !$tukaarak && !$hraashna) {
-quest::signalwith(128094,66,0);
-quest::shout("Warders, I have fallen. Prepare yourselves, these fools are determined to unleash doom!");
+  quest::signalwith(128094,66,0);
+  quest::shout("Warders, I have fallen. Prepare yourselves, these fools are determined to unleash doom!");
+  } else {
+  quest::shout("Warders, I have fallen. Prepare yourselves, these fools are determined to unleash doom!");
+  }
 }
-else { 
-quest::shout("Warders, I have fallen. Prepare yourselves, these fools are determined to unleash doom!");
-}
- }
