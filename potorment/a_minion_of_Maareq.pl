736da1943fb1fe67cda347f04bde759c60f25ce8
N0ctrnl <kurt@gilpin.info>
2018-02-11 03:44:08 -0600

    Adding leash to minion of Maareq

--- potorment/a_minion_of_Maareq.pl
+++ potorment/a_minion_of_Maareq.pl
@@ -1,15 +1,31 @@
+
 sub EVENT_SPAWN {
-
-quest::settimer(2,61);
-
+  $LeashedID = $entity_list->GetMobByNpcTypeID(207069);
+  quest::settimer(2,61);
+  quest::settimer(99,1);
 }
 
 sub EVENT_TIMER {
+  my $x = $npc->GetX();
+  my $y = $npc->GetY();
+  my $z = $npc->GetZ();
+  {
+    quest::stoptimer(99);
+    quest::settimer(99,1);
+  }
+  if($timer == 2) {
+    quest::depop();
+  }
 
-if($timer == 2) {
+  if ($timer == 99) {
+    if (!$LeashedID || $LeashedID->IsCorpse()) { #if the mob isn't up
+      return;
+    }
 
-quest::depop();
-
+    if ($x > 60 || $x < -90 || $y < -150 || $y > 6 || $z < 450 || $z > 480) {
+      $LeashedID->GMMove($SpawnX, $SpawnY, $SpawnZ, $SpawnH);
+    }
+  }
 }
 
-}
+
