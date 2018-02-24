c2e9b3de3bbd43fb296e88d3c500212235da1fb0
N0ctrnl <kurt@gilpin.info>
2018-02-24 01:03:53 -0600

    And VT

--- vexthal.bak/Zov_Centien.lua
+++
@@ -1,7 +0,0 @@
-function event_death(e)
-  local xloc = e.self:GetX();
-  local yloc = e.self:GetY();
-  local zloc = e.self:GetZ();
-  local heading = e.self:GetHeading();
-  eq.spawn2(eq.ChooseRandom(158099,158098,158097),0,0,xloc,yloc,zloc,heading);
-end
