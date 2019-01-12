0a5136b3b8870080a6e12375c843481be38cecea
N0ctrnl <kurt@gilpin.info>
2019-01-11 19:54:16 -0600

    Fix Aten spawn - Hopefully

--- vexthal/zone_controller.pl
+++ vexthal/zone_controller.pl
@@ -6,7 +6,8 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_TIMER {
-  if($timer eq "aten") {
+  if($timer eq "aten"){
+    quest::stoptimer("aten");
     if (!defined $qglobals{aten}) {
       if (!$entity_list->IsMobSpawnedByNpcTypeID(158014) && !$entity_list->IsMobSpawnedByNpcTypeID(158010) && !$entity_list->IsMobSpawnedByNpcTypeID(158015) && !$entity_list->IsMobSpawnedByNpcTypeID(158012) && !$entity_list->IsMobSpawnedByNpcTypeID(158013) && !$entity_list->IsMobSpawnedByNpcTypeID(158007) && !$entity_list->IsMobSpawnedByNpcTypeID(158008) && !$entity_list->IsMobSpawnedByNpcTypeID(158011) && !$entity_list->IsMobSpawnedByNpcTypeID(158009)) {
        quest::depopall(158006);
@@ -14,10 +15,19 @@ sub EVENT_TIMER {
           quest::spawn2(158096,0,0,1412,0,248.63,192);
   ##     quest::depop_withtimer();
         }
-        } elsif (!$entity_list->IsMobSpawnedByNpcTypeID(158006) && !$entity_list->IsMobSpawnedByNpcTypeID(158096)) {
-          quest::spawn2(158096,0,0,1412,0,248.63,192);
-        }
+      } elsif (!$entity_list->IsMobSpawnedByNpcTypeID(158006) && !$entity_list->IsMobSpawnedByNpcTypeID(158096)) {
+        quest::spawn2(158096,0,0,1412,0,248.63,192);
+      } else {
+        quest::settimer("aten",300);
+      }
     }
   }
-  quest::settimer("aten",600);
 }
+
+sub EVENT_SIGNAL {
+  if ($signal == 1) {
+    quest::stoptimer("aten");
+    quest::settimer("aten",300);
+  }
+}
+
